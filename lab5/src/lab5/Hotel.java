f83390b1321c0b6835efde45161f205d94d87d9f
ikhertsetseg <71216825+ikhertsetseg@users.noreply.github.com>
2021-03-08 22:45:00 +0800

    file upload

---
+++ lab5/src/lab5/Hotel.java
@@ -0,0 +1,24 @@
+package lab5;
+
+public class Hotel {
+
+	public int ID;
+	public int pNumber;
+	public String Name;
+	public String Address;
+	public String Roominfo;
+	public String Order;
+	public Hotel(int id, int pnumber, String name, String address, String roominfo, String order) {
+		ID = id;
+		pNumber = pnumber;
+		Name = name;
+		Address = address;
+		Roominfo = roominfo;
+		Order = order;
+		
+	}
+	public double totalRent() {
+		return 0;
+	}
+	
+}
---
+++ lab5/src/lab5/route.java
@@ -0,0 +1,14 @@
+package lab5;
+
+public class route {
+	public int ID;
+	public String Name;
+	
+	
+	public route(int id, String name) {
+		ID = id;
+		Name = name;
+		
+		
+	}
+}
